1a8d368b7dff3dfbccf941181a9012ef7182c9e3
ErColle <alessandrocolella205@gmail.com>
2025-12-02 14:48:19 +0100

    com

---
+++ src/Esercizi/esarray7.java
@@ -0,0 +1,9 @@
+package Esercizi;
+
+public class esarray7 {
+//    Esercizio n. 7 – elementi comuni
+//    Letti in input due array A e B, di lunghezza n ed m, stampare tutti gli elementi comuni ad A e B.
+    public static void main(String[] args){
+
+    }
+}
